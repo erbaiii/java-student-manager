9ad6e631f9ce27634ea47fd7abe7965610da6442
erbai <434341519@qq.com>
2023-06-11 21:16:04 +0800

    完善Student类

--- com.erbai/src/Main.java
+++ com.erbai/src/Main.java
@@ -1,9 +1,12 @@
 import com.erbai.Manager;
+import com.erbai.Student;
 
 
 public class Main {
     public static void main(String[] args) {
         Manager manager = new Manager("计算机应用技术","J001",4);
         System.out.println(manager.info());
+        Student student = new Student("二白","S01","男",18);
+        System.out.println(student.introduction());
     }
 }
