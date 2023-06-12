7f79418eb6f61db72a587806edc9af644e58199b
erbai <434341519@qq.com>
2023-06-12 22:04:27 +0800

    完善功能

--- com.erbai/src/Main.java
+++ com.erbai/src/Main.java
@@ -4,9 +4,12 @@ import com.erbai.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Manager manager = new Manager("计算机应用技术","J001",4);
+        Manager manager = new Manager("计算机应用技术", "J001", 4);
         System.out.println(manager.info());
-        Student student = new Student("二白","S01","男",18);
-        System.out.println(student.introduction());
+        Student student = new Student("二白", "S01", "男", 18);
+        System.out.println(student.introduction(manager.getSubjectName(), manager.getSubjectLife()));
+        System.out.println(student.introduction(manager));
+        Student student2 = new Student("二白", "S01", "男", 18, manager);
+        System.out.println(student2.introduction());
     }
 }
--- com.erbai/src/com/erbai/Student.java
+++ com.erbai/src/com/erbai/Student.java
@@ -4,9 +4,19 @@ public class Student {
     private String studentNo;
     private String studentName;
     private String studentSex;
+    private Manager myManager;
 
     public Student() {
 
+    }
+
+    public Manager getMyManager() {
+        if (this.myManager == null) this.myManager = new Manager();
+        return myManager;
+    }
+
+    public void setMyManager(Manager myManager) {
+        this.myManager = myManager;
     }
 
     public Student(String studentNo, String studentName, String studentSex, int studentAge) {
@@ -14,6 +24,14 @@ public class Student {
         this.setStudentName(studentName);
         this.setStudentSex(studentSex);
         this.setStudentAge(studentAge);
+        this.setMyManager(myManager);
+    }
+    public Student(String studentNo, String studentName, String studentSex, int studentAge, Manager myManager) {
+        this.setStudentNo(studentNo);
+        this.setStudentName(studentName);
+        this.setStudentSex(studentSex);
+        this.setStudentAge(studentAge);
+        this.setMyManager(myManager);
     }
 
     public String getStudentNo() {
@@ -50,8 +68,55 @@ public class Student {
 
     private int studentAge;
 
+    public String introduction(String subjectName, int subjectLife) {
+        String str = "" +
+                "学生信息如下：\n姓名："
+                + this.getStudentName()
+                + "\n学号："
+                + this.getStudentNo()
+                + "\n性别："
+                + this.getStudentSex()
+                + "\n年龄："
+                + this.getStudentAge()
+                + "\n所报专业名称："
+                + this.getMyManager().getSubjectName()
+                + "\n学制年限："
+                + this.getMyManager().getSubjectLife()
+                ;
+        return str;
+    }
+
+    public String introduction(Manager myManager) {
+        String str = "" +
+                "学生信息如下：\n姓名："
+                + this.getStudentName()
+                + "\n学号："
+                + this.getStudentNo()
+                + "\n性别："
+                + this.getStudentSex()
+                + "\n年龄："
+                + this.getStudentAge()
+                + "\n所报专业名称："
+                + myManager.getSubjectName()
+                + "\n学制年限："
+                + myManager.getSubjectLife();
+        return str;
+    }
     public String introduction() {
-        String str = "学生信息如下：\n姓名：" + this.getStudentName() + "\n学号：" + this.getStudentNo() + "\n性别：" + this.getStudentSex() + "\n年龄：" + this.getStudentAge();
+        String str = "" +
+                "学生信息如下：\n姓名："
+                + this.getStudentName()
+                + "\n学号："
+                + this.getStudentNo()
+                + "\n性别："
+                + this.getStudentSex()
+                + "\n年龄："
+                + this.getStudentAge()
+                + "\n所报专业名称："
+                + this.getMyManager().getSubjectName()
+                + "\n学制年限："
+                + this.getMyManager().getSubjectLife()
+                ;
         return str;
     }
 }
